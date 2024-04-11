7bbaa1894ab6f83cce1257ac104e0c5631bc2c5c
Jones Kwame Osei <oseikwamejones@gmail.com>
2024-04-11 03:20:46 +0100

    adds a provider definition file

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "jones-tf-state-bucket"
+    key    = "devops/infrastructure/resources.tfstate"
+    region = "eu-west-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.39.0"
+    }
+  }
+}
